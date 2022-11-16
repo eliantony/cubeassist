8df38482419f73d504d368c354267a4abca0964e
Sajay Antony <sajaya@microsoft.com>
2022-11-16 01:41:44 +0000

    Added drop down
    Signed-off-by: Sajay Antony <sajaya@microsoft.com>

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
-function App() {
-  return (
-    <div className="App">
+const options = [
+  'one', 'two', 'three'
+];
+const defaultOption = options[0];
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { value: 'Hello World' };
+  }
+   _onSelect = (e) => {
+    //alert(e.value);
+    //this.value = e.target.value;
+    this.setState({ value: e.value });
+  };
+
+  render() {
+    return (
+
+      <div className="App">
+  
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Profile />
-        <Game></Game>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Dropdown options={options}  
+              onChange={this._onSelect}
+              value={defaultOption} 
+              placeholder="Select an option" />
+        
+
+        <p>{this.state.value}</p> 
+   
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn about Cubeassist
         </a>
       </header>
     </div>
-  );
+     
+    );
+  }
 }
+
 
 function Profile() {
   return (
@@ -34,62 +62,6 @@ function Profile() {
   );
 }
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
 
 // ========================================
 
